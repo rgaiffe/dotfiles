4afdde24670d61fc0921aef0b7855a3eeb67608d
Remi GAIFFE <github@grumbleur.fr>
2023-06-27 21:38:02 +0200

    fix: if

--- shell/my_alias.sh
+++ shell/my_alias.sh
@@ -61,7 +61,7 @@ alias tmux="tmux -u"
 # pbcopy linux
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
-if [ "$(hostname)" == "laptop-hb" ] ; then
+if [ "$(hostname)" = "laptop-hb" ] ; then
   # openpvn3
   alias vpn-start="/usr/bin/openvpn3 session-start --config ${HOME}/work/laptop-hb_remi_gaiffe@hivebrite.openvpn.com_Paris.ovpn"
   alias vpn-status="/usr/bin/openvpn3 sessions-list"
